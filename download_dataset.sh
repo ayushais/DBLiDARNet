394d7aa968fc92bb2fdc587f963eb2bc1d3d3b6a
Ayush Dewan <dewan@informatik.uni-freiburg.de>
2019-06-13 10:46:41 +0200

    changed datasets file

--- download_dataset.sh
+++ download_dataset.sh
@@ -3,4 +3,6 @@ mkdir datasets
 cd datasets
 wget deep-temporal-seg.informatik.uni-freiburg.de/datasets/squeeze_seg.zip
 unzip squeeze_seg.zip
+mv squeeze_seg/* .
+rm -r squeeze_seg 
 rm squeeze_seg.zip
