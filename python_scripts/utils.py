362ddd5ee3bc41e30346115227cf03235b255e14
Ayush Dewan <dewan@informatik.uni-freiburg.de>
2019-06-14 12:55:55 +0200

    added license stuff

--- python_scripts/utils.py
+++ python_scripts/utils.py
@@ -1,4 +1,21 @@
 """utility functions"""
+  # This file is part of DBLIDARNet.
+ 
+  # Copyright (C) 2019 Ayush Dewan (University of Freiburg)
+ 
+  # DBLiDARNet is free software: you can redistribute it and/or modify
+  # it under the terms of the GNU General Public License as published by
+  # the Free Software Foundation, either version 3 of the License, or
+  # (at your option) any later version.
+ 
+  # DBLiDARNet is distributed in the hope that it will be useful,
+  # but WITHOUT ANY WARRANTY; without even the implied warranty of
+  # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  # GNU General Public License for more details.
+ 
+  # You should have received a copy of the GNU General Public License
+  # along with DBLiDARNet.  If not, see <https://www.gnu.org/licenses/>.
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
